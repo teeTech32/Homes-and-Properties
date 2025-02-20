7cdfd0c1fba8d8a8278ad754dabab1d65ebc6a0c
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-20 22:14:41 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,31 +5,25 @@ import { MdLocalOffer } from "react-icons/md";
 function Navbar() {
   const navigate = useNavigate()
 
- 
-
-  return (
-        <div class="flex justify-center bg-slate-800">
+ return (
           <div data-aos='zoom-in'
             data-aos-offset='200'
             data-aos-delay='200'
             data-aos-duration='2000'
-            data-aos-easing='ease-out-sine' class='flex justify-between m-5  gap-12 space-x-8 lg:gap-48 lg:space-x-56'>
-              <div class='group cursor-pointer pl-5 justify-center' onClick={() => navigate('/')}>
-                <FaHouseUser class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 pl-3 lg:pl-1.5" />
+            data-aos-easing='ease-out-sine' class='navbar bg-slate-800 p-1  relative'>
+              <div class='group cursor-pointer  absolute top-6 md:top-5 left-10 md:left-16' onClick={() => navigate('/')}>
+                <FaHouseUser class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 " />
                 <h4 class="text-white font-bold hover:text-green-500 lg:text-xl">Home</h4>
               </div>
-              <div class='group cursor-pointer' onClick={() => navigate('/offer')}>
-                <MdLocalOffer class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 pl-3 lg:pl-1.5"/>
-                <h4  class="text-white font-bold hover:text-green-500 lg:text-xl">Offers</h4>
+              <div class=' group cursor-pointer absolute top-6 md:top-5 right-[42%] md:right-[45%] flex justify-center' onClick={() => navigate('/offer')}>
+                <MdLocalOffer class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 "/>
+                <h4 class="text-white font-bold hover:text-green-500 lg:text-xl">Offers</h4>
               </div>
-              <div class='group cursor-pointer pr-5' onClick={() => navigate('/profile')}>
-                <FaUserAlt class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 pl-3 lg:pl-1.5"/>
+              <div class='group cursor-pointer  absolute top-6 md:top-5 right-10 md:right-16' onClick={() => navigate('/profile')}>
+                <FaUserAlt class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 "/>
                 <h4  class="text-white font-bold hover:text-green-500 lg:text-xl">Profile</h4>
               </div>
-            </div>
-          </div>
-        
+            </div> 
   )
 }
-
 export default Navbar
