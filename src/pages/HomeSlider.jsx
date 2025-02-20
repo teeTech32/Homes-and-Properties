12efaea0663245e765944afa329434fe2f88e935
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-20 22:20:47 +0100

    Update HomeSlider.jsx

--- src/pages/HomeSlider.jsx
+++ src/pages/HomeSlider.jsx
@@ -8,22 +8,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination'
 
-
 function HomeSlider() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
 
-  useEffect(()=>{
-    
+  useEffect(()=>{ 
   const fetchListings = async() =>{
-    
     const listingsRef = collection(db, 'listings')
     const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
     const querySnap = await getDocs(q) 
       let listings = []
       querySnap.forEach((doc) => {
-
         return listings.push({
           data: doc.data(),
           id: doc.id
@@ -35,18 +31,17 @@ function HomeSlider() {
   fetchListings()
   })
   
-
   if(loading){
     return <Spinner/>
   }
-
-  
   return (
-    
      listings && (
       <>
-        <p class="lg:text-2xl font-semibold lg:font-bold lg:py-5 py-3 sm:text-xl text-black ">Slide Left For Latest Recommended</p>
-
+        <p data-aos='fade-right'
+           data-aos-offset='300'
+           data-aos-delay='300'
+           data-aos-duration='3000'
+           data-aos-easing='ease-in-sine' class="lg:text-2xl font-semibold lg:font-bold lg:py-5 py-3 sm:text-xl text-black ">Slide Left For Latest Recommended</p>
         <Swiper  slidesPerView={1} pagination={{clickable: true}} modules={[Navigation, Pagination, Scrollbar, A11y]}>
           {listings.map(({data, id})=>(
             <SwiperSlide key={id} onClick={()=> navigate(`/category/${data.type}/${id}`)}>
@@ -55,25 +50,32 @@ function HomeSlider() {
                 backgroundSize:'cover', 
                 cursor: "pointer"
               }} className="swiperSlideDiv">
-                <p className="listingType">House For {data.type === 'rent' ? 'Rent' : 'Sale'}</p>
-                <p className="swiperSlideText">{data.name}</p>
-                <p className="swiperSlidePrice">
+                <p data-aos='zoom-in'
+                   data-aos-offset='300'
+                   data-aos-delay='300'
+                   data-aos-duration='2000'
+                   data-aos-easing='ease-in-sine' className="listingType">House For {data.type === 'rent' ? 'Rent' : 'Sale'}</p>
+                <p  data-aos='fade-right'
+                    data-aos-offset='300'
+                    data-aos-delay='300'
+                    data-aos-duration='3000'
+                    data-aos-easing='ease-in-sine' className="swiperSlideText">{data.name}</p>
+                <p data-aos='zoom-in'
+                   data-aos-offset='300'
+                   data-aos-delay='300'
+                   data-aos-duration='2000'
+                   data-aos-easing='ease-in-sine' className="swiperSlidePrice">
                   ${data.regularPrice
                     .toString()
                     .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
                   {' '}{data.type === 'rent' && '/ month'}
                   </p>
-             
               </div>
             </SwiperSlide>
           ))}
-
         </Swiper>
        </>
-     
-      
      )
-    
   )
 }
 
