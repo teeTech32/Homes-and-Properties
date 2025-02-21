4fbe69673b1742a481a5b54901d17c1b373b0f18
ATANDA TIMOTHY <timothyabidemi4618@gmail.com>
2025-02-21 03:22:16 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             data-aos-delay='200'
             data-aos-duration='2000'
             data-aos-easing='ease-out-sine' class='navbar bg-slate-800 p-1  relative'>
-              <div class='group cursor-pointer  absolute top-6 md:top-5 left-10 md:left-16' onClick={() => navigate('/')}>
+              <div class='group cursor-pointer  absolute top-6 md:top-5 left-5 md:left-10' onClick={() => navigate('/')}>
                 <FaHouseUser class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 " />
                 <h4 class="text-white font-bold hover:text-green-500 lg:text-xl">Home</h4>
               </div>
@@ -19,7 +19,7 @@ function Navbar() {
                 <MdLocalOffer class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 "/>
                 <h4 class="text-white font-bold hover:text-green-500 lg:text-xl">Offers</h4>
               </div>
-              <div class='group cursor-pointer  absolute top-6 md:top-5 right-10 md:right-16' onClick={() => navigate('/profile')}>
+              <div class='group cursor-pointer  absolute top-6 md:top-5 right-5 md:right-10' onClick={() => navigate('/profile')}>
                 <FaUserAlt class="text-white font-bold text-4xl lg:text-5xl hover:text-green-500 "/>
                 <h4  class="text-white font-bold hover:text-green-500 lg:text-xl">Profile</h4>
               </div>
